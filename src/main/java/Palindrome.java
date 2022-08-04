7a0e0e9891efcd64547537cf59d9209368b5f316
MutheuLinet <muneenilinet@gmail.com>
2022-08-04 08:17:47 +0300

    Add function to check if string is a palindrome

--- src/main/java/Palindrome.java
+++ src/main/java/Palindrome.java
@@ -1,2 +1,19 @@
 public class Palindrome {
+
+    //first get string input and reverse the string
+    //check if reversed string equals original sting, return true if palindrome.
+    public boolean isPalindrome(String original){
+        //initialise empty string variable to store the reversed string
+        String reversed = "";
+
+        for (int i=original.length()-1; i>=0; i--){
+            reversed = reversed + original.charAt(i);
+        }
+
+        if (original.equals(reversed)){
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
