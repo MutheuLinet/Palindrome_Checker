77eca852b3e54e94f415c2f6f4f96b01abe9ce78
MutheuLinet <muneenilinet@gmail.com>
2022-08-04 08:15:42 +0300

    Collect input and check if its a palindrome

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,2 +1,14 @@
+import java.util.Scanner;
+
 public class App {
+    public static void main(String [] args){
+
+        System.out.println("Please enter a name to check if it is a palindrome: ");
+        Scanner scanner = new Scanner(System.in);
+        String inputtedName = scanner.nextLine();   //collect user input
+
+        Palindrome palindrome = new Palindrome();
+        System.out.println(palindrome.isPalindrome(inputtedName)); // check if collected string is a palindrome
+
+    }
 }
